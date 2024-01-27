6e81d256a51061a91acf7a7de12a2e035560d5e1
ozalpm <ozalpm1@gmail.com>
2024-01-27 04:48:24 +0300

    düt düüüt

--- Assets/Mustafa/Scripts/Npc.cs
+++ Assets/Mustafa/Scripts/Npc.cs
@@ -15,16 +15,19 @@ public class Npc : MonoBehaviour
             case QuestionsReaction.Bad:
                 Invoke(nameof(BreakTheTalk),data.reactionClips[0].length);
                 source.clip = data.reactionClips[0];
+                GameManager.Init.popularity--;
                 source.Play();
                 break;
             case QuestionsReaction.Normal:
                 Invoke(nameof(BreakTheTalk),data.reactionClips[1].length);
                 source.clip = data.reactionClips[1];
+                GameManager.Init.popularity++;
                 source.Play();
                 break;
             case QuestionsReaction.Good:
                 Invoke(nameof(BreakTheTalk),data.reactionClips[2].length);
                 source.clip = data.reactionClips[2];
+                GameManager.Init.popularity+=2;
                 source.Play();
                 break;
             default:
